23af7eb73256b3cefab4ea665fb519cd4a5cba40
pqphong187 <phongpqf8197@fullstack.edu.vn>
2025-01-02 20:16:28 +0700

    day 26

--- homework/day26/ex.js
+++ homework/day26/ex.js
@@ -4,38 +4,47 @@ Năm 2009: ECMAScript 5 ra đời với nhiều cải tiến quan trọng như c
 Năm 2015:ECMAScript 6 (ES6) ra đời,là phiên bản lớn nhất của ECMAScript với nhiều cải tiến và tính năng được coi là vượt bậc.
 Năm 2016 - nay:Các phiên bản ECMAScript mới được ra mắt liên tục,với nhiều cải tiến và tính năng mới.Cho đến nay javaScript đã trở thành một trong số những ngôn ngữ được ưa chuộng và sử dụng nhiều nhất thế giới.`;
 
-
 function printHighlight(content, keyword) {
-    const contentLower = content.toLowerCase();
-    const keywordLower = keyword.toLowerCase();
+  if (typeof content !== "string" || content.trim() === "") {
+    return `Nội dung không hợp lệ.`;
+  }
+  if (typeof keyword !== "string" || keyword.trim() === "") {
+    return `Từ khóa không hợp lệ."`;
+  }
 
-    let start = 0;
-    let highlightedContent = "";
+  const contentLower = content.toLowerCase();
+  const keywordLower = keyword.toLowerCase();
 
-    while (true) {
-        start = contentLower.indexOf(keywordLower, start);
-        if (start === -1) {
-            highlightedContent += content.slice(highlightedContent.length);
-            break;
-        } else {
-            highlightedContent += content.slice(highlightedContent.length, start) + '<b>' + content.slice(start, start + keyword.length) + '</b>';
-            start += keyword.length;
-        }
+  let start = 0;
+  let highlightedContent = "";
+
+  while (true) {
+    start = contentLower.indexOf(keywordLower, start);
+    if (start === -1) {
+      highlightedContent += content.slice(highlightedContent.length);
+      break;
+    } else {
+      highlightedContent +=
+        content.slice(highlightedContent.length, start) +
+        "<b>" +
+        content.slice(start, start + keyword.length) +
+        "</b>";
+      start += keyword.length;
     }
-    console.log(highlightedContent);
+  }
+  return highlightedContent;
 }
 
 function fixContent(content) {
-    content = content.replace(/([.,;:!?])(?=\S)/g, "$1 ");
+  content = content.replace(/([.,;:!?])(?=\S)/g, "$1 ");
 
-    content = content.replace(/(\S)([({[\]<])/g, "$1 $2");
-    content = content.replace(/([)}\]>])(\S)/g, "$1 $2"); 
+  content = content.replace(/(\S)([({[\]<])/g, "$1 $2");
+  content = content.replace(/([)}\]>])(\S)/g, "$1 $2");
 
-    content = content.replace(/\s+/g, ' ').trim();
+  content = content.replace(/\s+/g, " ").trim();
 
-    return content;
+  return content;
 }
 
-
-printHighlight(jsContent, "javascript");
+console.log(printHighlight(jsContent, "javascript"));
 console.log(fixContent(jsContent));
